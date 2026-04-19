82a6e2866edd4cac07fda0a60ea5fc6e3037f47d
hudsonmp <70823510+hudsonmp@users.noreply.github.com>
2026-04-19 03:36:50 +0000

    Deploying to gh-pages from @ hudsonmp/hudsonmp.github.io@04f5f97b7fca11da22a39b3f61da8dc0ee479202 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/library/";
           },
-        },{id: "post-transforming-societies",
+        },{id: "post-little-me-part-i",
+        
+          title: 'Little Me, Part I <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "Trying to reflect on years 1-15...",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://hudsonmp.substack.com/p/little-me-part-i", "_blank");
+          
+        },
+      },{id: "post-transforming-societies",
         
           title: "Transforming Societies",
         
